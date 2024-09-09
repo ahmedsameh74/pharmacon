ac6576d4432ddbbc7a9de37a533cf569cbfafc5d
Ahmed Sameh <ahmed.sameh@zeyada.net>
2024-09-10 02:20:33 +0300

    responsive, interactive navbar

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,15 @@
+import Navbar from "@/components/custom/Navbar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
+    <Navbar/>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold text-center">Ahmed</h1>
+        <Button variant="link">Click</Button>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -98,5 +103,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+    </>
   );
 }
