cb659f64e9bc68d054698bd19fa307fdb29fe19e
Ahmed Sameh <ahmed.sameh@zeyada.net>
2024-09-13 02:31:57 +0300

    finalized about page

--- src/components/custom/home/ContactSection.tsx
+++ src/components/custom/home/ContactSection.tsx
@@ -2,7 +2,7 @@ export default function ContactSection() {
   return (
     <div className="max-w-4xl mx-auto my-8 p-6 bg-[#01547E] flex flex-col justify-center items-center">
       {/* Title */}
-      <h1 className="text-xl sm:text-2xl lg:text-3xl text-center font-bold text-white my-4 px-4 md:px-0">
+      <h1 className="text-xl sm:text-2xl lg:text-3xl text-center font-bold text-white my-4 px-4 md:px-0 max-w-lg">
         Get Your Free Medication Review. Let&apos;s Connect With Us!
       </h1>
 
