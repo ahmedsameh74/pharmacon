4772503efab642fde7c6e0f492fd9f0209ec800e
Ahmed Sameh <ahmed.sameh@zeyada.net>
2024-10-01 14:50:08 +0300

    imgs

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,8 @@
-import dynamic from 'next/dynamic';
-
 // import About from '@/components/custom/home/About';
 // import ContactSection from '@/components/custom/home/ContactSection';
+import dynamic from 'next/dynamic';
+import { Metadata } from 'next';
+
 import Hero from '@/components/custom/home/Hero';
 import Products from '@/components/custom/home/Products';
 import StatsBanner from '@/components/custom/home/StatsBanner';
@@ -19,6 +20,59 @@ const AboutDynamic = dynamic(() => import('@/components/custom/home/About'), {
 //     ssr: false
 //   }
 // );
+
+export const metadata: Metadata = {
+  title: 'Pharmacon',
+  description: 'Because Health Deserves Quality',
+  icons: {
+    icon: '/assets/logo icon.ico',
+    apple: '/assets/blueLogo.png'
+  },
+  manifest: '/manifest.webmanifest',
+  keywords: [
+    'Pharmacon',
+    'PharmaconEG',
+    'EG',
+    'eg',
+    'Health',
+    'Quality',
+    'Products',
+    'Pharmacy',
+    'فارماكون'
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
+  },
+  openGraph: {
+    title: 'Pharmacon',
+    description: 'Because Health Deserves Quality',
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+    siteName: 'Pharmacon',
+    images: [
+      {
+        url: '/assets/blueLogo.svg',
+        width: 800,
+        height: 600
+      }
+    ],
+    locale: 'en-US',
+    type: 'website'
+  },
+  twitter: {
+    title: 'Pharmacon',
+    description: 'Because Health Deserves Quality',
+    card: 'summary_large_image',
+    images: ['/assets/blueLogo.svg']
+  }
+};
 
 export default function Home() {
   return (
